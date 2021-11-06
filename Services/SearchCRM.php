84695f3c1cee94c708de9a03c8608b141180f88c
aurelien3333 <aurelienverdeau@gmail.com>
2021-11-06 18:08:29 +0100

    wip

--- Services/SearchCRM.php
+++ Services/SearchCRM.php
@@ -5,7 +5,6 @@ namespace Modules\SearchCRM\Services;
 
 
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Support\Facades\Gate;
 use Modules\SearchCRM\Contracts\SearchContract;
 use Modules\SearchCRM\Entities\CollectionSearchResults;
 use Modules\SearchCRM\Interfaces\SearchableModel;
@@ -35,26 +34,24 @@ class SearchCRM implements SearchContract
         if($value) {
             foreach ($this->repositories as $repository) {
                 $repository = app($repository);
-                if(Gate::allows('view', $repository->getModel())) {
-                    $query = $repository->newQuery();
-                    foreach (explode(' ', $value) as $lem) {
-                        $query->where(function (Builder $query) use ($repository, $lem) {
-                            $query->setQuery(
-                                $repository->searchQuery($query, $lem)
-                                    ->getQuery()
-                            );
-                        });
-                    }
-
-                    $results->push(
-                        ...$query
-                        ->limit($limit)
-                        ->get()
-                        ->map(function (SearchableModel $item) {
-                            return $item->getSearchResult();
-                        })
-                    );
+                $query = $repository->newQuery();
+                foreach(explode(' ', $value) as $lem) {
+                    $query->where(function(Builder $query) use ($repository, $lem){
+                        $query->setQuery(
+                            $repository->searchQuery($query, $lem)
+                            ->getQuery()
+                        );
+                    });
                 }
+
+                $results->push(
+                    ...$query
+                    ->limit($limit)
+                    ->get()
+                    ->map(function (SearchableModel $item) {
+                        return $item->getSearchResult();
+                    })
+                );
             }
         }
 
